2b3b8cc6c32143a58756ad45cc6754a600bcacc7
luchomellu <luchomellu@live.com.ar>
2024-06-21 20:29:28 -0300

    Arboles "Terminado" (Falta ultimo punto pero es al pedo)

--- src/practico_02A/main.java
+++ src/practico_02A/main.java
@@ -2,6 +2,7 @@ package practico_02A;
 
 public class main {
 	public static void main(String[] args) {
+		/*
 		Tree arbol = new Tree();
 		
 		arbol.add(76);
@@ -40,6 +41,44 @@ public class main {
 		arbol.ej2();
 		arbol.imprimirArbol();
 		
+		*/
+		//Ejercicio6
+		
+		CharTree arbolEj6 = new CharTree();
+		CharTreeNode uno = new CharTreeNode('M');
+		CharTreeNode dos = new CharTreeNode('A');
+		CharTreeNode tres = new CharTreeNode('L');
+		CharTreeNode cuatro = new CharTreeNode('N');
+		CharTreeNode cinco = new CharTreeNode('A');
+		CharTreeNode seis = new CharTreeNode('O');
+		CharTreeNode siete = new CharTreeNode('I');
+		CharTreeNode ocho = new CharTreeNode('S');
+		CharTreeNode nueve = new CharTreeNode('A');
+		CharTreeNode diez = new CharTreeNode('O');
+		
+		uno.setLeft(dos);
+		
+		dos.setLeft(tres);
+		dos.setRight(cuatro);
+		
+		cuatro.setLeft(cinco);
+		cuatro.setRight(seis);
+		
+		uno.setRight(siete);
+		
+		siete.setLeft(ocho);
+		ocho.setLeft(nueve);
+		
+		siete.setRight(diez);
+		
+		arbolEj6.setRoot(uno);
+		arbolEj6.imprimirArbol();
+		
+		System.out.println("Nro Hoja 0: " + arbolEj6.ej6(0));
+		System.out.println("Nro Hoja 1: " + arbolEj6.ej6(1));
+		System.out.println("Nro Hoja 2: " + arbolEj6.ej6(2));
+		System.out.println("Nro Hoja 3: " + arbolEj6.ej6(3));
+		System.out.println("Nro Hoja 4: " + arbolEj6.ej6(4));
 	}
 	
 }
